347c8b6729bcc2c45422c0616e55d032d8782f5b
김현준 <69852555+kimhyeonjun-96@users.noreply.github.com>
2024-01-31 20:17:48 +0900

    Boj1138 설명 위해 주석 달기, Boj 17478 문제 풀이

--- myCode/src/boj/구현/mySolution/한줄로서기_1138.java
+++ myCode/src/boj/구현/mySolution/한줄로서기_1138.java
@@ -10,13 +10,19 @@ public class 한줄로서기_1138 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine()); // 사람 수
-        int[] line = new int[N]; // 실제 줄 위치를 저장
+        int[] line = new int[N]; // 실제 사람이 서 있었 던리에 사람을 저장 -> 출력
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++){
+        for(int i=0; i<N; i++){ // i = 현재의 사람
             int front = Integer.parseInt(st.nextToken()); // i번째 사람의 앞에 있는 큰 사람들의 수
+
+            System.out.println((i+1) + "번째 사람, 왼쪽 앞에 있는 사람의 수 = " + front);
+
             int idx = findPosition(line, front, N); // i번째 사람의 실제 위치 탐색
-            line[idx] = i + 1;
+
+            System.out.println((i+1) + "번째 사람이 실제로 있었던 자리 = " + (idx+1));
+
+            line[idx] = i + 1; // 실제로 있었던 자리에 사람을 저장
         }
         // 출력
         for (int i : line) {
@@ -27,7 +33,10 @@ public class 한줄로서기_1138 {
 
     private static int findPosition(int[] line, int front, int n) {
         for (int i = 0; i < n; i++) {
-            if (front == 0 && line[i] == 0) { // 현재 사람이 앞에 front 수만큼 있는 위치로 간다면
+
+            System.out.println(line[i]);
+
+            if (front == 0 && line[i] == 0) { // 현재 사람이 front 수만큼 키가큰 사람이 앞에 있고 위치에 아무도 서있지 않다면 위치로 간다면
                 return i; // 위치를 반환
             }
             if(line[i] == 0) front--; // 앞의 사람의 수가 1 이상이고 줄에 사람이 없다면 앞의 사람 수를 줄인다. 즉  줄에 설 수 있어도 앞의 사람의 수가 맞지 않으면 뒤로 더 가야한다.
